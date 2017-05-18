06a804b2274adc1f2b2cc20acfed56f7b27276f7
pengJi <hi_jipeng@163.com>
2017-05-18 17:37:02 +0800

    sss

--- application/controllers/Wechat.php
+++ application/controllers/Wechat.php
@@ -15,22 +15,35 @@ class Wechat extends MY_Controller {
 		
 		# 加载对应操作接口
 		//文件夹名注意大写
-		$this->load->library('wechat_user', $config);
+		// $this->load->library('wechat_user', $config);
 		
 	}
 	
 	public function index(){
 		
 		# 配置参数
-		$config = array(
+		/*$config = array(
 			'token'          => 'dapeng',
 			'appid'          => 'wx3431de4713ceb3bb',
-			'appsecret'      => '39ea2b90c55ec14462b1967909316895',
-			'encodingaeskey' => '',
+			// 'appsecret'      => '39ea2b90c55ec14462b1967909316895',
+			'encodingaeskey' => 'e1Z1Rp855zB15oD5GEOibGBOsv1q80osuL1g1Q9blEd',
 		);
 
 		# 加载对应操作接口
-		$this->wechat->get('User', $config);
+		$this->wechat->get('User', $config);*/
 		//$userlist = $wechat->getUserList();
+		$this -> _valid();
 	}
+	public function _valid(){    
+        $token = 'dapeng'; 
+        $signature = $this->input->get('signature'); 
+        $timestamp = $this->input->get('timestamp'); 
+        $nonce = $this->input->get('nonce'); 
+        $echostr = $this->input->get('echostr'); 
+        $tmp_arr = array($token, $timestamp, $nonce); 
+        sort($tmp_arr); 
+        $tmp_str = implode($tmp_arr); 
+        $tmp_str = sha1($tmp_str); 
+        return ($tmp_str==$signature); 
+    }
 }
