dd23f7caedef73ce216ced309ee6ffcae994ee95
pengJi <hi_jipeng@163.com>
2017-05-18 16:42:29 +0800

    ssss

--- application/controllers/Wechat.php
+++ application/controllers/Wechat.php
@@ -13,7 +13,7 @@ class Wechat extends MY_Controller {
     	$token = 'dapeng';
     	$encodingAesKey = 'e1Z1Rp855zB15oD5GEOibGBOsv1q80osuL1g1Q9blEd';
     	$corpId = 'wx3431de4713ceb3bb';
-        $this -> load -> library('WXBizMsgCrypt',$token, $encodingAesKey, $corpId);
+        $this -> load -> library('WXBizMsgCrypt',array($token, $encodingAesKey, $corpId));
 		// $wxcpt = new WXBizMsgCrypt($token, $encodingAesKey, $corpId);
 		$errCode = $this -> wxbizmsgcrypt -> VerifyURL($sVerifyMsgSig, $sVerifyTimeStamp, $sVerifyNonce, $sVerifyEchoStr, $sEchoStr);
     }
