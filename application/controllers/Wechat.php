62dbae88196d726f05e45cd3539effbcba505465
pengJi <hi_jipeng@163.com>
2017-05-18 17:22:12 +0800

    222

--- application/controllers/Wechat.php
+++ application/controllers/Wechat.php
@@ -3,20 +3,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Wechat extends MY_Controller {
 
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	public function __construct() {
+		parent::__construct();
+		
+		# 配置参数
+		$config = array(
+		'token'          => 'dapeng',
+		'appid'          => 'wx3431de4713ceb3bb',
+		'encodingaeskey' => 'e1Z1Rp855zB15oD5GEOibGBOsv1q80osuL1g1Q9blEd',
+		);
+		
+		# 加载对应操作接口
+		//文件夹名注意大写
+		$this->load->library('wechat_user', $config);
+		
+	}
+	
+	public function index(){
+		
+		# 配置参数
+		$config = array(
+			'token'          => 'dapeng',
+			'appid'          => 'wx3431de4713ceb3bb',
+			'appsecret'      => '39ea2b90c55ec14462b1967909316895',
+			'encodingaeskey' => '',
+		);
 
-    public function index()
-    {
-    	$token = 'dapeng';
-    	$encodingAesKey = 'e1Z1Rp855zB15oD5GEOibGBOsv1q80osuL1g1Q9blEd';
-    	$corpId = 'wx3431de4713ceb3bb';
-        $this -> load -> library('Wechat');
-        $options['token'] = $token;
-        $options['encodingaeskey'] = $encodingAesKey;
-        $options['appid'] = $corpId;
-		$this -> wechat -> valid();
-    }
+		# 加载对应操作接口
+		$this->wechat->get('User', $config);
+		//$userlist = $wechat->getUserList();
+	}
 }
