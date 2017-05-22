c85e0591cbcc316e6b53c1ded8ff443875095452
cosin_lu <cosin_lu@163.com>
2017-05-22 22:50:56 +0800

    微信

--- application/controllers/Wechat.php
+++ application/controllers/Wechat.php
@@ -15,31 +15,21 @@ class Wechat extends MY_Controller {
 		
 		# 加载对应操作接口
 		//文件夹名注意大写
-		// $this->load->library('wechat_user', $config);
+        $this->load->library('wechat_receive');
 		
 	}
 	
 	public function index(){
-		
-		# 配置参数
-		/*$config = array(
-			'token'          => 'dapeng',
-			'appid'          => 'wx3431de4713ceb3bb',
-			// 'appsecret'      => '39ea2b90c55ec14462b1967909316895',
-			'encodingaeskey' => 'e1Z1Rp855zB15oD5GEOibGBOsv1q80osuL1g1Q9blEd',
-		);
-
-		# 加载对应操作接口
-		$this->wechat->get('User', $config);*/
-		//$userlist = $wechat->getUserList();
-		$this -> valid();
+        $type   = $this->getRev ()
+            ->getRevType ();
+        $openid = $this->getRevFrom ();
+        phone_log($type);
+        phone_log($openid);
 	}
 
     public function valid()
    {
       $echoStr = $_GET["echostr"];
-
-      //valid signature , option
       if($this->checkSignature()){
          echo $echoStr;
          exit;
