01d7136691eadc1e8418c976a0f1960407550b1e
cosin_lu <cosin_lu@163.com>
2017-05-22 23:17:51 +0800

    www;

--- application/controllers/Wechat.php
+++ application/controllers/Wechat.php
@@ -15,7 +15,7 @@ class Wechat extends MY_Controller {
 		
 		# 加载对应操作接口
 		//文件夹名注意大写
-        $this->load->library('wechat_receive');
+        $this->load->library('wechat_receive',$config);
 		
 	}
 	
