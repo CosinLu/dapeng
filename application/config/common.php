6e4388d2016d96dd78495cc897682d7e7b2a4615
pengJi <hi_jipeng@163.com>
2017-05-23 13:58:22 +0800

    common

---
+++ application/config/common.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * 常量配置
+ *
+ * @author Cosin
+ */
+class common
+{
+    const MSGTYPE_TEXT					= 'text';
+    const EVENT_SUBSCRIBE 				= 'subscribe';       //订阅
+    const EVENT_UNSUBSCRIBE 			= 'unsubscribe';   //取消订阅
+}
