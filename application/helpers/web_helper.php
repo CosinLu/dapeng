30d63e5d4b44b41817990f229322e61c010e8b28
pengJi <hi_jipeng@163.com>
2017-05-24 16:50:23 +0800

    sss

--- application/helpers/web_helper.php
+++ application/helpers/web_helper.php
@@ -16,11 +16,46 @@
     }
 
     //生成激活码
-    function make_code(){
+    /*function make_code(){
         $str = 'ABCDEFuvwGH012IJKLMNOPQpqrstR56STUVWXYZabcdefghijklmnoxyz34789';
         $length = strlen($str) - 7;
         $start = rand(0,$length);
         $code = substr($str,$start,6);
+        return $code;
+    }*/
+    function make_code(){
+        $arr = array(
+            'A'=>1,
+            'B'=>2,
+            'C'=>3,
+            'D'=>4,
+            'E'=>5,
+            'F'=>6,
+            'G'=>7,
+            'H'=>8,
+            'I'=>9,
+            'J'=>10,
+            'K'=>11,
+            'L'=>12,
+            'M'=>13,
+            'N'=>14,
+            'O'=>15,
+            'P'=>16,
+            'Q'=>17,
+            'R'=>18,
+            'S'=>19,
+            'T'=>20,
+            'U'=>21,
+            'V'=>22,
+            'W'=>23,
+            'X'=>24,
+            'Y'=>25,
+            'Z'=>26);
+        $rand_code1 = array_rand($arr,4);
+        $rand_code2 = array_rand($arr,4);
+        $rand_code3 = array_rand($arr,4);
+        $rand_code4 = array_rand($arr,4);
+        $code = implode('',$rand_code1).'-'.implode('',$rand_code2).'-'.implode('',$rand_code3).'-'.implode('',$rand_code4);
         return $code;
     }
 
