ea9822eae1149b24dc002422287b43219ad5a9ea
pengJi <hi_jipeng@163.com>
2017-05-24 14:18:57 +0800

    sss;

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -15,4 +15,36 @@ class User extends MY_Controller {
     	$this -> assign('user_data',$user_data);
         $this -> display('user/index.html');
     }
+
+    public function opera_status(){
+    	$status = 0;
+    	$id = $this -> getParams('id');
+    	$action = $this -> getParams('action');
+    	if($action == 'dis')
+			$status = 2;    
+		if($action == 'cancel'){
+	    	$this -> load -> model('invit_code_model','code');
+	    	$where['uid'] = $id;
+	    	$is_active = $this -> code -> getOneByCondition($where);
+	    	if($is_active)
+	    		$status = 1;
+		}
+		$data['status'] = $status;
+		$res = $this -> user -> update($id,$data);		
+		if($res){
+			$result = array('code'=>200,'message'=>'操作成功');
+			echo json_encode($result);exit;
+		}else{
+			$result = array('code'=>500,'message'=>'操作失败');
+			echo json_encode($result);exit;
+		}	
+    }
+
+
+
+
+
+
+
+
 }
