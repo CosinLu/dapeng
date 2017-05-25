e89fcebb29abbd48f0212f196ced31d82523697f
pengJi <hi_jipeng@163.com>
2017-05-25 18:30:01 +0800

    sss

--- application/controllers/Matter_manage.php
+++ application/controllers/Matter_manage.php
@@ -16,4 +16,12 @@ class Matter_manage extends MY_Controller {
         $this -> assign('datas',$datas);
         $this -> display('matter_manage/index.html');
     }
+
+    public function add_matter(){
+        $this -> load -> model('write_person_model','write');
+        $condition['status'] = 0;
+        $write_person_datas = $this -> write -> getAllByCondition($condition);
+        $this -> assign('write_person_datas',$write_person_datas);
+        $this -> display('matter_manage/add_matter.html');
+    }
 }
