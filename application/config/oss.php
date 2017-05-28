ceb0f91aef9b699a11b77956b898c935d7b7865c
cosin_lu <cosin_lu@163.com>
2017-05-28 17:01:30 +0800

    测试

--- application/config/oss.php
+++ application/config/oss.php
@@ -3,10 +3,9 @@
 /**
  * Oss SDK Config File
  */
-$config['oss_access_id']    = 'y6mFW2kNvP1e5UdH';
-$config['oss_access_key']   = 'cg0ILBKd3kRP1o1wleHLvsfBk8bUwf';
-$config['oss_bj_host']      = 'oss-cn-beijing.aliyuncs.com';
-$config['oss_hz_host']      = 'oss-cn-hangzhou-a.aliyuncs.com';
+$config['oss_access_id']    = 'LTAIx1oVSs03onRc';
+$config['oss_access_key']   = 'QgAw7X74tOlCDjads7Bww35XN5AOBU';
+$config['oss_bj_host']      = 'cosinlu.oss-cn-beijing.aliyuncs.com';
 $config['ali_log']          = FALSE;
 $config['ali_display_log']  = FALSE;
 $config['ali_lang']         = 'zh';
