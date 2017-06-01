a5edf1b1559e6a41cab1198a2a0cd37bbc09dd1a
pengJi <hi_jipeng@163.com>
2017-06-01 15:28:12 +0800

    sss

--- application/controllers/Matter_manage.php
+++ application/controllers/Matter_manage.php
@@ -26,25 +26,70 @@ class Matter_manage extends MY_Controller {
     }
 
     public function do_add_matter(){
-        if ($_FILES['img_path'])
-        {
-            $filePath = $_FILES['img_path']['tmp_name'];
+        $field_name = $this -> getParams('field_name');
+        if(empty($field_name)){
+            echo "<script>alert('字不能为空')</script>";
+            redirect('/matter_manage/index');
+        }
+        $params['field_name'] = $field_name;
+        $description = $this -> getParams('description');
+        if(empty($description)){
+            echo "<script>alert('描述不能为空')</script>";
+            redirect('/matter_manage/index');
+        }
+        $params['description'] = $description;
+        if ($_FILES['img_path']){
+            $file_path = $_FILES['img_path']['tmp_name'];
             $suf_name = explode('.',$_FILES['img_path']['name']);
-            $fileName = md5($_FILES['img_path']['name'] . time()).'.'.$suf_name[1];
+            $file_name = md5($_FILES['img_path']['name'] . time()).'.'.$suf_name[1];
 
-            if (!empty($filePath))
+            if (!empty($file_path))
             {
                 $this->load->library('oss/alioss');
-                $response = $this -> alioss -> upload_file_by_file('cosinlu',$fileName,$filePath);
-                dd($response);
+                $response = $this -> alioss -> upload_file_by_file($this -> bucket,$file_name,$file_path);
                 if ($response['status'] == 200)
                 {
-                    $paramter['url'] = $fileName;
-                    $paramter['createline'] = time();
+                    $img_path = $this -> alioss -> get_sign_url($this -> bucket,$file_name);
+                    $params['img_path'] = $img_path;
+                }else{
+                    echo "<script>alert('图片上传失败')</script>";
+                    redirect('/matter_manage/index');
                 }
+            }else{
+                echo "<script>alert('图片不能为空')</script>";
+                redirect('/matter_manage/index');
             }
         }
-        dd($a);
+
+        if ($_FILES['gif_path']){
+            $filePath = $_FILES['gif_path']['tmp_name'];
+            $sufName = explode('.',$_FILES['gif_path']['name']);
+            $fileName = md5($_FILES['gif_path']['name'] . time()).'.'.$sufName[1];
+
+            if (!empty($filePath)){
+                $this->load->library('oss/alioss');
+                $response = $this -> alioss -> upload_file_by_file($this -> bucket,$fileName,$filePath);
+                if ($response['status'] == 200){
+                    $imgPath = $this -> alioss -> get_sign_url($this -> bucket,$file_name);
+                    $params['gif_path'] = $imgPath;
+                }else{
+                    echo "<script>alert('gif图上传失败')</script>";
+                    redirect('/matter_manage/index');
+                }
+            }else{
+                echo "<script>alert('gif图不能为空')</script>";
+                redirect('/matter_manage/index');
+            }
+        }
+
+        $res = $this -> matter -> insert($params);
+        if($res){
+            echo "<script>alert('添加成功')</script>";
+            redirect('/matter_manage/index');
+        }else{
+            echo "<script>alert('添加失败')</script>";
+            redirect('/matter_manage/index');
+        }
     }
 
     public function file_upload(){
