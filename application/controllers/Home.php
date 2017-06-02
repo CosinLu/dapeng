942d6d9b13734b11713f1631f060f35c9492b247
pengJi <hi_jipeng@163.com>
2017-06-02 14:55:27 +0800

    sss

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,15 +1,42 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends MY_Controller {
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
+class Home extends CI_Controller {
 
     public function index()
     {
-        echo 111;
+        $this -> load -> view('home/login.html');
+    }
+
+    public function do_login(){
+    	$name = $this -> input -> post('name');
+    	$passwd = md5($this -> input -> post('passwd'));
+    	if(!empty($name) && !empty($passwd)){
+    		$this -> load -> model('admin_model','admin');
+    		$condition['name'] = $name;
+    		$condition['passwd'] = $passwd;
+    		$res = $this -> admin -> getOneByCondition($condition);
+    		if($res){
+    			$this -> session -> name = $res['name'];
+    			$this -> session -> id = $res['id'];
+    			redirect('/admin/index');
+    		}else{
+    			echo "<script>alert('登录失败');
+            window.location.href='/home/index';
+            </script>";
+            exit;
+    		}
+    	}else{
+            echo "<script>alert('登录失败');
+            window.location.href='/home/index';
+            </script>";
+            exit;
+    	}
+    }
+
+    public function logout(){
+    	$sess_data = array('id','name');
+    	$this->session->unset_userdata($sess_data);
+    	redirect('/home/index');
     }
 }
