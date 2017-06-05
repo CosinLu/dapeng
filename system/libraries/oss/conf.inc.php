f8a24b652a475b80cc22bf5b1b47e9244ee91226
pengJi <hi_jipeng@163.com>
2017-06-05 13:17:21 +0800

    sss

--- system/libraries/oss/conf.inc.php
+++ system/libraries/oss/conf.inc.php
@@ -11,7 +11,7 @@ define('OSS_ACCESS_KEY', 'dogJ7mwYPFE67kxexv7YroCmHHqtle');
 define('ALI_LOG', false);
 
 //自定义日志路径，如果没有设置，则使用系统默认路径，在./logs/
-//define('ALI_LOG_PATH','');
+define('ALI_LOG_PATH',APPPATH.'/logs');
 
 //是否显示LOG输出
 define('ALI_DISPLAY_LOG',false);
